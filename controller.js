7d8a1cb8e3584c1bdc65480681e799744e3e9673
Paige Viehweg <browneyesallen@gmail.com>
2016-02-16 19:37:40 -0700

    made it all work

--- controller.js
+++ controller.js
@@ -1,12 +1,23 @@
 var app = angular.module('userProfiles');
 
 app.controller('MainController', function($scope, mainService) {
+ 
+ $scope.currentPage = 1;
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+ $scope.getUsers = function() {
+   mainService.getUsers($scope.currentPage).then(function(response) {
+     $scope.users = response.data;
+   });
+ }
+ $scope.getUsers();
 
+ $scope.next = function() {
+     $scope.currentPage++;
+     $scope.getUsers();
+ }
 
-  $scope.getUsers();
-
+  $scope.previous = function() {
+     $scope.currentPage--;
+     $scope.getUsers();
+ }
 });
